d322e6f9489e4ec53a59cbc2b5372c57214b7234
SergeyRushev <rushevs198417@gmail.com>
2023-07-06 10:59:12 +0300

    create migrations

---
+++ database/migrations/2023_07_06_073553_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('list_id');
+            $table->foreign('list_id')->references('id')->on('lists');
+            $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->boolean('completed')->default(false);
+            $table->timestamp('due_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
